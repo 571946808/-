b4b61b2764bb53341341a3605ecfb84e760bb603
zhangxiangnan <571946808@qq.com>
2017-04-10 10:55:44 +0800

    完善

--- js/accordion.js
+++ js/accordion.js
@@ -1,26 +1,50 @@
-$(function() {
-    var Accordion = function(el, multiple) {
-        this.el = el || {};
-        this.multiple = multiple || false;
+$(function(){
+// The `enabled` flag will be `false` if CSS 3D isn't available
 
-        // Variables privadas
-        var links = this.el.find('.link');
-        // Evento
-        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    if ( $.fn.makisu.enabled ) {
+
+        var $sashimi = $( '.sashimi' );
+        var $nigiri = $( '.nigiri' );
+        var $maki = $( '.maki' );
+
+        // Create Makisus
+
+        $nigiri.makisu({
+            selector: 'dd',
+            overlap: 0.85,
+            speed: 1.7
+        });
+
+        $maki.makisu({
+            selector: 'dd',
+            overlap: 0.6,
+            speed: 0.85
+        });
+
+        $sashimi.makisu({
+            selector: 'dd',
+            overlap: 0.2,
+            speed: 0.5
+        });
+
+        // Open all
+        
+        $( '.list' ).makisu( 'open' );
+
+        // Toggle on click
+
+        $( '.toggle' ).on( 'click', function() {
+            $( '.list' ).makisu( 'toggle' );
+        });
+
+        // Disable all links
+
+        $( '.demo a' ).click( function( event ) {
+            event.preventDefault();
+        });
+
+    } else {
+
+        $( '.warning' ).show();
     }
-
-    Accordion.prototype.dropdown = function(e) {
-        var $el = e.data.el;
-            $this = $(this),
-            $next = $this.next();
-
-        $next.slideToggle();
-        $this.parent().toggleClass('open');
-
-        if (!e.data.multiple) {
-            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-        };
-    }   
-
-    var accordion = new Accordion($('#accordion'), false);
 });
